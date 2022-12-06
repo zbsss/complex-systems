b716da847684ca51d411d8ee3f12619f91355d2d
Michał Kurleto <mkurleto@qualtrics.com>
2022-12-06 14:48:08 +0100

    Working graph random walks

--- project/random_walks.py
+++ project/random_walks.py
@@ -2,73 +2,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-# import scipy.sparse.csr_matrix
 
-def generate_ba(n, m, m0):
-    """
-    n - number of nodes
-    m0 - number of isolated nodes at time 0,
-    m - degree of new node, m <= m0
-    """
-    graph = nx.Graph()
 
-    graph.add_nodes_from(range(m0))
+graph = nx.karate_club_graph()
 
-    for j in range(m0, n):
-        """
-        probability of edge (j, i) is proportional to degree of node i
-        """
+nx.draw(graph)
+plt.show()
 
-        degrees = [deg + 1 for _, deg in graph.degree]
-
-        total = sum(degrees)
-
-        neighbor = np.random.choice(graph.nodes(), size=m, p=[deg / total for deg in degrees])
-
-        for i in neighbor:
-            graph.add_edge(j, i)
-
-    return graph
-
-graph = generate_ba(10**2, 3, 3)
 A = nx.adjacency_matrix(graph).toarray()
-# graph = nx.from_numpy_matrix(A)
 
 N = len(A)
 
 PI = np.ones((N, N))
-PI /= np.sum(PI, axis=0)
+PI /= np.sum(PI, axis=1)
 
-i = np.random.randint(N)
+j = np.random.randint(N)
 
-heatmap = np.zeros((N, N))
+heatmap = np.zeros(N)
 
-alpha = 1
+alpha = 0.5
+
 
 for t in range(100):
-  """
-  p(i, t) - probability that the walker occupies node i at time t
+    """
+    p(i, t) - probability that the walker occupies node i at time t
 
-  PI[i, j]:
-      - transition matrix of probabilities moving from node j to node i as time t
-      - columns must sum to 1
-  """
-  while i == (j := np.random.choice(range(N), p=PI[i])): continue
-  heatmap[i, j] += 1
-  
-  div = 1
-  for l in range(N):
-    div += A[l, j] * np.exp(alpha * np.sum(PI[l]))
+    PI[i, j]:
+        - transition matrix of probabilities moving from node j to node i as time t
+        - columns must sum to 1
+    """
+    p = PI @ PI[:, j]
 
-  PI[i, j] = A[i, j] * np.exp(alpha * np.sum(PI[i])) / div
-  
-  i = j
+    while j == (i := np.random.choice(range(N), p=p)):
+        continue
+    heatmap[i] += 1
+
+    print(f"{j} -> {i}")
+
+    div = 0
+    for l in range(N):
+        div += A[l, j] * np.exp(alpha * p[l])
+
+    for k in range(N):
+        PI[k, j] = (A[k, j] * np.exp(alpha * p[k])) / div
+
+    j = i
 
 
 print(heatmap)
 
-
-
-
-plt.imshow(heatmap)
+nx.draw(graph, node_color=heatmap, cmap='hot')
 plt.show()
